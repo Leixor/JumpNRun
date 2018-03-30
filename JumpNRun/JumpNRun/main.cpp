f3804f708050d36b4da422295df2bf5032182dfb
VTric <VTric@LAPTOP-VH43KDSD>
2018-03-30 17:08:53 +0200

    Change

--- JumpNRun/JumpNRun/main.cpp
+++ JumpNRun/JumpNRun/main.cpp
@@ -1,5 +1,23 @@
 #pragma once
-#include "standardInclude.h"
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include <iostream>
+#include <mutex>
+
+#include <stdio.h>
+#include <tchar.h>
+
+#include <SFML\Graphics.hpp>
+
+#include <Windows.h>
+
+using namespace std;
+using namespace sf;
 
 int main()
 {
