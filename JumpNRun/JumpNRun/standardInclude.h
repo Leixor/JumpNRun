6cfefd4ab4a6bbfed8d45c9faacd17bf31259fc1
Zusendo <masterzusendo@gmail.com>
2018-03-30 17:04:36 +0200

    StandardInclude

---
+++ JumpNRun/JumpNRun/standardInclude.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <iostream>
+#include <mutex>
+
+#include <stdio.h>
+#include <tchar.h>
+
+#include <SFML\Graphics.hpp>
+
+#include <Windows.h>
+
+using namespace std;
+using namespace sf;
