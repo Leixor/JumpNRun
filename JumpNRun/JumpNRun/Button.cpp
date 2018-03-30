28ae308f414359c8861d01b54e7bce18b02279a1
Zusendo <masterzusendo@gmail.com>
2018-03-30 19:01:12 +0200

    FirstDay

---
+++ JumpNRun/JumpNRun/Button.cpp
@@ -0,0 +1,20 @@
+#include "standardInclude.h"
+
+Button::Button(IntRect buttonRect, void (*buttonAction)())
+	: buttonShape(), buttonAction(buttonAction)
+{
+	this->buttonShape.setPosition(buttonRect.left, buttonRect.top);
+	this->buttonShape.setSize(Vector2f(buttonRect.width, buttonRect.height));
+
+}
+
+Button::~Button()
+{
+}
+
+void Button::checkMouseButtonCollision(float mouseX, float mouseY)
+{
+	if (this->buttonShape.getGlobalBounds().contains(Vector2f(mouseX, mouseY)))
+		this->buttonAction();
+	
+}
--- JumpNRun/JumpNRun/main.cpp
+++ JumpNRun/JumpNRun/main.cpp
@@ -1,19 +1,31 @@
 #pragma once
 #include "standardInclude.h"
 
+void game()
+{
+	printf("HI");
+}
+
 int main()
 {
 	RenderWindow window(VideoMode(400, 400), "Jump and Run!");
 	RectangleShape shape(Vector2f(100.0f, 100.0f));
 	shape.setFillColor(Color::Red);
 
+	Button tmp(IntRect(0, 0, 400, 400), game);
+	tmp.checkMouseButtonCollision(10, 10);
+
 	while (window.isOpen())
 	{
 		Event event;
 		while (window.pollEvent(event))
 		{
-			if (event.type == Event::Closed)
+			switch (event.type)
+			{
+			case Event::Closed:
 				window.close();
+				break;
+			}
 		}
 
 		window.clear();
