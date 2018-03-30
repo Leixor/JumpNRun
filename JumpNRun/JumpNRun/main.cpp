5aff56077c1ea15e48833852a16b8ebca0815ce4
Zusendo <masterzusendo@gmail.com>
2018-03-30 16:47:26 +0200

    SFMLBasic

--- JumpNRun/JumpNRun/main.cpp
+++ JumpNRun/JumpNRun/main.cpp
@@ -1,4 +1,43 @@
-void main()
+#pragma once
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include <iostream>
+#include <mutex>
+
+#include <stdio.h>
+#include <tchar.h>
+
+#include <SFML\Graphics.hpp>
+
+#include <Windows.h>
+
+using namespace std;
+using namespace sf;
+
+int main()
 {
-	// Krasser Cod
+	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
+	return 0;
 }
